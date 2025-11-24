80ff5bf1613ebab44f2919f4eb5d1e800ebe277c
tech with Ayush <techwithayushhhh@gmail.com>
2025-11-24 22:18:48 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
 import './App.css'
-import GetMovies from './components/getMovies'
-import Header from './components/Header'
-import Home from './components/Home'
+
 import Layout from './Layout'
 
 function App() {
